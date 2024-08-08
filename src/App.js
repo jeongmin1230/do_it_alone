5a1237591457cee0cb5be82e0e9115b60913cc97
jeongmin <tmxk0810@gmail.com>
2024-08-08 13:59:27 +0900

    생활 코딩에서 배운 것과 다르게 구현 하다보니 더 헷갈려져서.. 작성 했던 코드 다 날리고 다시 시작함.. 기존 topics 리스트를 화면에 보여주는 것 구현
    2024. 8. 8(목)

--- src/App.js
+++ src/App.js
@@ -1,88 +1,40 @@
 import './App.css';
 import { useState } from 'react';
-// create 는 첫 화면에 버튼으로
-// read 는 첫 화면에 바로
-// update 는 첫 화면에서 특정 글 눌렀을 때 버튼 나오게
-// delete 도 특정 글 눌렀을 때 버튼으로
 
-function Menu(props) {
+function Show(props) {
   return <div>
-    <a href={''} onClick={event => {
-      event.preventDefault();
-      props.onCreate();
-    }}>작성</a>
+    <h3>{props.title}</h3>
+    <h5>{props.body}</h5>
   </div>
 }
 
-function Create(props) {
-  return <div>
-    <form onSubmit={event => {
-      event.preventDefault();
-      const title = event.target.title.value;
-      const content = event.target.body.value;
-      props.onCreate(title, content);
-    }}>
-      <p><input type='text' name='title' placeholder='title'/></p>
-      <p><textarea name='body' placeholder='body'></textarea></p>
-      <p><input type='submit' value='작성'></input></p>
-    </form>
-  </div>
-}
-
-function Read(props) {
-  const lis = []
-  for(let i=0; i<props.topics.length; i++){
-    let t = props.topics[i];
-    lis.push(<li key={t.id}>
-      <a id={t.id} href={'/read/'+t.id} onClick={event=>{
+function List(props) {
+  const lis = [];
+  for(let i = 0; i < props.topics.length; i++) {
+    let t = props.topics[i]
+    lis.push(<li key = {t.id}>
+      <a id = {t.id} href={'/read/' + t.id} onClick={event => {
         event.preventDefault();
-        props.onChangeMode(Number(event.target.id));
       }}>{t.title}</a>
     </li>)
   }
   return <div>
-    <ol>
+    <ul>
       {lis}
-    </ol>
+    </ul>
   </div>
 }
 
 function App() {
-  const [mode, setMode] = useState('READ');
-  const [topics, setTopics] = useState([
-    {id:1, title: '제목 1', content: '내용 1'}
-  ])
+  const [topics, setTopics] = useState([{id: 1, title: '제목', body: '내용입니다.'}]);
+  const [mode, setMode] = useState('WELCOME');
   const [id, setId] = useState(null);
-  const [nextId, setNextId] = useState(topics.length+1);
   let content = null;
-  if(mode === 'CREATE') {
-    content = <Create onCreate={(_title, _body) => {
-      const newTopic = {id: nextId, title: _title, content: _body};
-      const newTopics = [...topics];
-      newTopics.push(newTopic);
-      setTopics(newTopics);
-      setMode('READ');
-      setId(nextId);
-      setNextId(nextId+1);
-    }}></Create>
-  } else if(mode === 'READ') {
-
-  } else if(mode === 'UPDATE') {
-
-  } else if(mode === 'DELETE') {
-
-  }
-
-  return (
-    <div className='body'>
-      <Menu onCreate={() => {
-        if(mode==='READ') setMode('CREATE');
-        else if(mode==='CREATE') setMode('READ');
-      }}></Menu>
-      <Read topics={topics}></Read>
-      {content}
-    </div>
-  );
+  return <div className='body'>
+    <Show title = 'Review' body = 'react app'></Show>
+    <List topics={topics}></List>
+    {content}
+  </div>
 }
 
 export default App;
